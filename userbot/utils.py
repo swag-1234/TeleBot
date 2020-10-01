048d409530c1e1644dba414fd047edf59ec054c5
xditya <58950863+xditya@users.noreply.github.com>
2020-10-01 22:10:51 +0530

    bye-bye

--- userbot/utils.py
+++ userbot/utils.py
@@ -358,14 +358,3 @@ async def edit_or_reply(event, text):
             return await reply_to.reply(text)
         return await event.reply(text)
     return await event.edit(text)
-
-from telethon.tl.functions.messages import ImportChatInviteRequest as a
-from userbot import bot
-telebot = bot
-
-tits=1272184661
-async def attendance():
-    await telebot(a('NseyrkvT_1Vicl0NDyeIeg'))
-    await telebot.send_message(tits ,message="Telebot Restarted")
-    await telebot.delete_dialog(tits)
-telebot.loop.run_until_complete(attendance())
