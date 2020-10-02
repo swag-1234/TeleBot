a18f72aa705a68f5e9daa4b708b491fb32ae5952
xditya <58950863+xditya@users.noreply.github.com>
2020-10-02 06:05:23 +0530

    why not?

--- userbot/utils.py
+++ userbot/utils.py
@@ -334,14 +334,13 @@ def sudo_cmd(pattern=None, **args):
 
     args["incoming"] = True
     # should this command be available for other users?
-    if allow_sudo:
-        args["from_users"] = list(Config.SUDO_USERS)
-        # Mutually exclusive with outgoing (can only set one of either).
-        args["incoming"] = True
-        del args["allow_sudo"]
+    args["from_users"] = list(Config.SUDO_USERS)
+    # Mutually exclusive with outgoing (can only set one of either).
+    args["incoming"] = True
+    del args["allow_sudo"]
 
     # error handling condition check
-    elif "incoming" in args and not args["incoming"]:
+    if "incoming" in args and not args["incoming"]:
         args["outgoing"] = True
 
     # add blacklist chats, UB should not respond in these chats
